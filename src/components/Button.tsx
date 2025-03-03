866f2a17e76e7e1883ca89672bed25acbc9fff96
tawiah <tawiahin4k@gmail.com>
2025-03-03 11:47:38 +0000

    add Button component with customizable styles and props

---
+++ src/components/Button.tsx
@@ -0,0 +1,25 @@
+interface ButtonProps {
+  text: string;
+  white?: boolean;
+  className: string;
+}
+
+const Button = ({text, white, className}: ButtonProps) => {
+  return (
+    <button>
+      <a
+        href="#_"
+        className={
+          `block ${className} text-base` +
+          (white
+            ? " text-[#1F1F1F] bg-[#CCCCCC] font-medium hover:bg-[#CCCCCC]/80"
+            : " text-white bg-[#1F1F1F]/40 hover:bg-[#1F1F1F] border border-[#CCCCCC]/20")
+        }
+      >
+        {text}
+      </a>
+    </button>
+  );
+}
+
+export default Button
