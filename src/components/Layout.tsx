9a16a8b7ffd47a4a950f3662bb22756f33ffb81f
tawiah <tawiahin4k@gmail.com>
2025-03-04 15:05:19 +0000

    :art: enhance Layout component with new icons and improved content structure for better user engagement

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,6 @@
 /** @format */
 
+import { GitMerge, Globe, MessageCircle, Terminal } from "lucide-react";
 import Button from "./Button";
 
 const Layout = () => {
@@ -40,11 +41,20 @@ const Layout = () => {
         </div>
       </div>
       <div className="flex flex-col items-center justify-center bg-white dark:bg-black w-full gap-4 mx-auto px-4 bottom-10">
-        <div className="border-l border-r border-black/[0.2] group/canvas-card flex items-center justify-center dark:border-white/[0.2] xl:max-w-7/12 relative w-full mx-auto  h-[20rem]">
-          <h1 className="text-3xl font-medium text-white/90 text-center">
-            Develop with your favorite tools
-            <br /> Launch globally, instantly Keep pushing
-          </h1>
+        <div className="border-l border-r border-black/[0.2] group/canvas-card flex items-center justify-center dark:border-white/[0.2] xl:max-w-7/12 relative w-full mx-auto h-[20rem]">
+          <div className="text-center flex flex-col items-center">
+            <div className="flex items-center justify-center gap-2">
+              <h1 className="text-3xl font-medium text-white/90">
+                Develop with your favorite tools
+              </h1>
+              <Terminal size={40} className="text-white mt-2" />
+            </div>
+            <h1 className="text-3xl font-medium text-white/90 mt-2 flex items-center justify-center gap-2">
+              Launch globally, <Globe size={30} className="text-white" />{" "}
+              instantly Keep pushing{" "}
+              <GitMerge size={30} className="text-white" />
+            </h1>
+          </div>
         </div>
       </div>
       <div className="flex flex-col items-center justify-center bg-white dark:bg-black w-full gap-4 mx-auto px-4 bottom-10">
@@ -57,14 +67,37 @@ const Layout = () => {
       </div>
       <div className="flex flex-col items-center justify-center bg-white dark:bg-black w-full gap-4 mx-auto px-4 bottom-10">
         <div className="border-l border-r border-b border-black/[0.2] group/canvas-card items-center justify-center dark:border-white/[0.2] xl:max-w-7/12 relative w-full mx-auto h-[35rem] grid grid-cols-2">
-          <h1 className="text-3xl font-medium text-white/90 text-center border-r border-white/[0.2]">
-            Develop with your favorite tools
-            <br /> Launch globally, instantly Keep pushing
-          </h1>
-          <h1 className="text-3xl font-medium text-white/90 text-center">
-            Develop with your favorite tools
-            <br /> Launch globally, instantly Keep pushing
-          </h1>
+          <div className="border-r border-black/[0.2] dark:border-white/[0.2] h-full">
+            <div className="px-15 py-10">
+              <h1 className="text-[#CCCCCC] text-lg flex gap-1">
+                <Terminal className=" mb-2" />
+                Git-connected Deploys
+              </h1>
+              <h1 className="text-white text-2xl font-bold">
+                From localhost to https, in seconds.
+              </h1>
+              <h1 className="text-[#CCCCCC] text-2xl">
+                Deploy from Git or your CLI.
+              </h1>
+            </div>
+            <img src="/shipping.png" alt="" className="mx-auto select-none" />
+          </div>
+          <div className="border-black/[0.2] dark:border-white/[0.2] h-full">
+            <div className="px-15 py-10">
+              <h1 className="text-[#CCCCCC] text-lg flex gap-1">
+                <MessageCircle className=" mb-2" />
+                Collaborative pre-production
+              </h1>
+              <h1 className="text-white text-2xl font-bold">
+                Every deploy is remarkable.{" "}
+                <span className="text-[#CCCCCC] font-normal">
+                  Chat with your team on real, production-grade UI, not just
+                  designs.
+                </span>
+              </h1>
+            </div>
+            <img src="/message.png" alt="" className="mx-auto select-none" />
+          </div>
         </div>
       </div>
       <div className="flex flex-col items-center justify-center bg-white dark:bg-black w-full gap-4 mx-auto px-4 bottom-10">
@@ -107,7 +140,9 @@ const Layout = () => {
 
 export default Layout;
 
-export const Icon = ({ className, ...rest }: any) => {
+import { SVGProps } from "react";
+
+export const Icon = ({ className, ...rest }: { className?: string } & SVGProps<SVGSVGElement>) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
