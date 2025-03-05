a0ff4791a53142442fb768a358f72700afb10d72
tawiah <tawiahin4k@gmail.com>
2025-03-05 13:47:14 +0000

    :fire: add FooterLinks constant with structured links for improved footer navigation

---
+++ src/constant/FooterLinks.ts
@@ -0,0 +1,71 @@
+/** @format */
+
+import { Github, Linkedin, Twitter, Youtube } from "lucide-react";
+
+export const Footerlinks = [
+  {
+    title: "Products",
+    links: [
+      "AI",
+      "Enterprise",
+      "Fluid Compute",
+      "Next.js",
+      "Observability",
+      "Previews",
+      "Rendering",
+      "Security",
+      "Turbo",
+      "v0",
+    ],
+  },
+  {
+    title: "Resources",
+    links: [
+      "Community",
+      "Docs",
+      "Guides",
+      "Help",
+      "Integrations",
+      "Pricing",
+      "Resources",
+      "Solution Partners",
+      "Templates",
+    ],
+  },
+  {
+    title: "Company",
+    links: [
+      "About",
+      "Blog",
+      "Careers",
+      "Changelog",
+      "Contact Us",
+      "Customers",
+      "Events",
+      "Partners",
+      "Privacy Policy",
+      "Legal",
+    ],
+  },
+  {
+    title: "Social",
+    links: [
+      {
+        company: "GitHub",
+        icon: Github,
+      },
+      {
+        company: "Linkedin",
+        icon: Linkedin,
+      },
+      {
+        company: "Twitter",
+        icon: Twitter,
+      },
+      {
+        company: "Youtube",
+        icon: Youtube,
+      },
+    ],
+  },
+];
