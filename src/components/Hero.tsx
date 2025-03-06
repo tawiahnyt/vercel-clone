33b98e409d262a9e40c2123a7313ae3ae8392667
tawiah <tawiahin4k@gmail.com>
2025-03-06 19:48:38 +0000

    :art: enhance Hero component with improved layout, new text content, and button interactions for better user engagement

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,16 +1,42 @@
 /** @format */
 
 import { SVGProps } from "react";
+import Button from "./Button";
 
 const Hero = () => {
   return (
-    <div className="pt-40 flex flex-col lg:flex-row items-center justify-center bg-white dark:bg-black w-full gap-4 mx-auto px-8">
-      {/* <div className="border border-black/[0.2] group/canvas-card items-center justify-center dark:border-white/[0.2]  max-w-6xl w-full mx-auto p-4 relative h-[40rem]">
+    <div className="pt-40 flex flex-col items-center justify-center bg-white dark:bg-black w-full gap-4 mx-auto px-4">
+      <div className="border border-black/[0.2] group/canvas-card flex items-center justify-center dark:border-white/[0.2] xl:max-w-7/12 w-full mx-auto relative h-[45rem]">
         <Icon className="absolute h-10 w-10 -top-5 -left-5 text-white/40" />
-        <Icon className="absolute h-10 w-10 -bottom-5 -left-5 text-white/40" />
-        <Icon className="absolute h-10 w-10 -top-5 -right-5 text-white/40" />
         <Icon className="absolute h-10 w-10 -bottom-5 -right-5 text-white/40" />
-      </div> */}
+
+        <div className=" text-center">
+          <h1 className="text-4xl font-bold text-white/90">
+            Your complete platform for the web.
+          </h1>
+          <p className="pt-7 text-xl leading-10 text-[#CCCCCC]">
+            Vercel provides the developer tools and cloud infrastructure <br />{" "}
+            to build, scale, and secure a faster, more personalized web.
+          </p>
+          <div className="mt-10 space-x-4">
+            <Button
+              white
+              text={"Start Deploying"}
+              className="rounded-full px-10 py-4"
+            />
+            <Button
+              text="Get a Demo"
+              className="rounded-full px-12 py-4 font-medium"
+            />
+          </div>
+
+          <img
+            src="/vercel-icon-light.png"
+            alt=""
+            className="mt-15 h-40 mx-auto"
+          />
+        </div>
+      </div>
     </div>
   );
 };
